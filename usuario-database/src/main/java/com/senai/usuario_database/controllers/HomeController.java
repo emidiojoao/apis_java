3fbe47da518e291e73c684c6476bf4b2a1ba0518
JOÃO CARLOS EMÍDIO <joao_c_emidio@estudante.sesisenai.org.br>
2025-04-10 18:48:57 -0300

    refatora o nome da controller de home

--- usuario-database/src/main/java/com/senai/usuario_database/controllers/HomeController.java
+++ usuario-database/src/main/java/com/senai/usuario_database/controllers/HomeController.java
@@ -1,13 +1,12 @@
 package com.senai.usuario_database.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/home")
-public class UsuarioHome {
+public class HomeController {
 
     @GetMapping
     public String obterHome(){
