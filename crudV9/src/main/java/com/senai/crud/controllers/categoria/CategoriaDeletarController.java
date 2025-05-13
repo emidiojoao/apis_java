3e6ac6debcd8550f84c10fc7b5e2de2eb41b1d4e
JOÃO CARLOS EMÍDIO <joao_c_emidio@estudante.sesisenai.org.br>
2025-05-13 18:44:33 -0300

    cria controller de deletar categoria

---
+++ crudV9/src/main/java/com/senai/crud/controllers/categoria/CategoriaDeletarController.java
@@ -0,0 +1,26 @@
+package com.senai.crud.controllers.categoria;
+
+import com.senai.crud.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/deletar-categoria")
+public class CategoriaDeletarController {
+
+    @Autowired
+    CategoriaService service;
+
+    public ResponseEntity<Boolean> deletarCategoria(@PathVariable Long id){
+
+        Boolean sucesso = service.deletarCategoria(id);
+        if(sucesso){
+            return ResponseEntity.ok().body(true);
+        }
+
+        return ResponseEntity.ok().body(false);
+    }
+}
