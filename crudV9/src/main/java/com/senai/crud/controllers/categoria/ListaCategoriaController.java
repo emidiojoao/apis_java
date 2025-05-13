0b51cdcab52bcedfd118c49f9a885827074e12c0
JOÃO CARLOS EMÍDIO <joao_c_emidio@estudante.sesisenai.org.br>
2025-05-12 21:39:20 -0300

    cria controller de listar categoria

---
+++ crudV9/src/main/java/com/senai/crud/controllers/categoria/ListaCategoriaController.java
@@ -0,0 +1,29 @@
+package com.senai.crud.controllers.categoria;
+
+import com.senai.crud.dtos.categoria.ListaCategoriaDTO;
+import com.senai.crud.services.CategoriaService;
+import com.senai.crud.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/lista-categoria")
+public class ListaCategoriaController {
+
+    @Autowired
+    CategoriaService service;
+
+    @GetMapping
+    public String obterListaCategoria(Model model){
+
+        List<ListaCategoriaDTO> listaCategoriaDTO = service.listaCategoria();
+        model.addAttribute("listaCategotiaDTO", listaCategoriaDTO);
+
+        return "categorialista";
+    }
+}
