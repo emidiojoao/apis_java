b34f3b91f7c8a89976e96f2d5e705fb15a39a6b9
JOÃO CARLOS EMÍDIO <joao_c_emidio@estudante.sesisenai.org.br>
2025-05-13 18:52:44 -0300

    altera rota para script de deletar e adiciona anotacao na controller de deletar categoria

--- crudV9/src/main/java/com/senai/crud/controllers/categoria/CategoriaDeletarController.java
+++ crudV9/src/main/java/com/senai/crud/controllers/categoria/CategoriaDeletarController.java
@@ -4,6 +4,7 @@ import com.senai.crud.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,6 +15,7 @@ public class CategoriaDeletarController {
     @Autowired
     CategoriaService service;
 
+    @DeleteMapping("/{id}")
     public ResponseEntity<Boolean> deletarCategoria(@PathVariable Long id){
 
         Boolean sucesso = service.deletarCategoria(id);
