57d067fa190a83af7a6516c2da3fae5f5ce6638c
emidiojoao <joaojoaocarlos045@gmail.com>
2025-05-19 20:03:36 -0300

    criao sessao na controller de listar produto

--- crudV9/src/main/java/com/senai/crud/controllers/produto/ProdutoListarController.java
+++ crudV9/src/main/java/com/senai/crud/controllers/produto/ProdutoListarController.java
@@ -1,7 +1,10 @@
 package com.senai.crud.controllers.produto;
 
 import com.senai.crud.dtos.produto.ListarProdutoDTO;
+import com.senai.crud.dtos.usuario.UsuarioSessaoDTO;
 import com.senai.crud.services.ProdutoService;
+import com.senai.crud.sessao.ControleSessao;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,7 +19,13 @@ public class ProdutoListarController {
     ProdutoService service;
 
     @GetMapping("/lista-produto")
-    public String obterProdutoLista(Model model){
+    public String obterProdutoLista(Model model, HttpServletRequest requisicao){
+
+        UsuarioSessaoDTO usuarioSessaoDTO = ControleSessao.obter(requisicao);
+        if(usuarioSessaoDTO.getId() == 0){
+
+            return "redirect:/login";
+        }
 
         List<ListarProdutoDTO> listarProdutoDTO = service.listarProdutos();
         model.addAttribute("listarProdutoDTO", listarProdutoDTO);
