2d2a7736036f5bc83fbadf9a0c0fb3f07a1e120d
emidiojoao <joaojoaocarlos045@gmail.com>
2025-05-19 20:56:54 -0300

    cria sessao na controller de cadastro de categoria

--- crudV9/src/main/java/com/senai/crud/controllers/categoria/CategoriaCadastrarController.java
+++ crudV9/src/main/java/com/senai/crud/controllers/categoria/CategoriaCadastrarController.java
@@ -1,8 +1,11 @@
 package com.senai.crud.controllers.categoria;
 
 import com.senai.crud.dtos.categoria.RequisicaoCategoriaDTO;
+import com.senai.crud.dtos.usuario.UsuarioSessaoDTO;
 import com.senai.crud.exception.InvalidOperationException;
 import com.senai.crud.services.CategoriaService;
+import com.senai.crud.sessao.ControleSessao;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,7 +20,12 @@ public class CategoriaCadastrarController {
     CategoriaService service;
 
     @GetMapping
-    public String obterCadastro(Model model){
+    public String obterCadastro(Model model, HttpServletRequest requisicao){
+
+        UsuarioSessaoDTO usuarioSessaoDTO = ControleSessao.obter(requisicao);
+        if(usuarioSessaoDTO.getId() == 0){
+            return "redirect:/login";
+        }
 
         RequisicaoCategoriaDTO requisicaoCategoriaDTO = new RequisicaoCategoriaDTO();
         model.addAttribute("requisicaoCategoriaDTO", requisicaoCategoriaDTO);
